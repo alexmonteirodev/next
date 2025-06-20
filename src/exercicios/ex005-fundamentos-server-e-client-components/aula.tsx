b88906644d7b4ccdb4d1bd347d4b7c63216634fb
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-06-20 15:44:38 +0200

    Server e Client Components part 2

--- src/exercicios/ex005-fundamentos-server-e-client-components/aula.tsx
+++ src/exercicios/ex005-fundamentos-server-e-client-components/aula.tsx
@@ -65,3 +65,41 @@ export default function Width() {
 // agora usando um client component você tem acesso a toda a API web: document, window, api's do react (useState, useEffect...), tudo que vc já está acostumado a usar.
 
 //obs: components do tipo servidor deixam importar components do tipo client. Já o Cliente não deixa importar do tipo server. (pq o client não tem acesso ao server)
+//---------------------------------------------------------------------------------
+
+// - Nested Components
+//componente dentro de componente, como funciona?
+// Server Components podem conter Client e Server Components, mas Client Components não podem conter Server Components.
+
+// Evite definir páginas (page.tsx) como Client Components, mantenha elas sempre como Server Components, assim elas podem receber Server e Client Components.
+
+//se precisar de uma page que seja client, crie um componente
+//---------------------------------------------------------------------------------
+
+// - Pré-renderização
+// Client Components são pré-renderizados no servidor, durante a pré-renderização, não temos acesso a APIs web como window e document. Por isso erros como document is not defined podem ocorrer.
+
+// é dizer, se tenho um componente que diz: <h2>largura total da tela: {total}</h2>, o h2 está renderizado no servidor, ou seja, ja faz parte do backend, então se desabilitarmos o JS na pagina, vai continuar aparecendo o h2, poruqe o total vai ficar em 0, porque isso sim era calculado via JS.
+
+// Se o código estiver dentro do useEffect, ele só será ativado no cliente, por isso não teremos problemas. Mas códigos fora do useEffect podem causar erros.
+//ex: se em um componente vc escrever console.log(document.documentElement.clientWidth), vai dar um erro 'document is not defined', porque vc está rodando código no server e é como se ele buscasse esse comando dentro do node, o que não existe, só existe no browser, porém quando o código está dentro de um useEffect ele renderiza no client, por isso funciona dentro do useEffect.
+//obs: fica atendo quando importar algo ou usar um hook, porque caso essa importacao vá utilizar de cara algum elemento da API web como document, vai dar erro por estar fora do effect. Ex:
+//const [largura, setLargura] = React.useState(documentElement.clientWidth) //erro
+
+//caso precise usar algo da Api web fora do effect, da pra desabilitar a pré-renderização daquele código em específico da pra usar o 'dynamic', funcionalidade nativa do Next e desabilita a pre-renderização (ssr: false)
+// solução de importação:
+
+import dynamic from "next/dynamic";
+
+export const Width = dynamic(() => import("@/components/width"), {
+  ssr: false,
+});
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <Width />
+    </main>
+  );
+}
